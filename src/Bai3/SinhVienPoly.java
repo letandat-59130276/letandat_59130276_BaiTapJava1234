b48eea24063610a71176cced96350817f3e1802d
Con Meo Cutee <Con Meo Cutee@DESKTOP-024VCJP>
2020-04-12 01:59:20 +0700

    fix 1.5

--- src/Bai3/SinhVienPoly.java
+++ src/Bai3/SinhVienPoly.java
@@ -9,33 +9,37 @@ package Bai3;
  *
  * @author Con Meo Cutee
  */
-abstract public class SinhVienPoly {
+abstract public class SinhVienPoly 
+{
     private String hoTen;
     private String nganh;
 
-    public SinhVienPoly(String hoTen, String nganh) {
+    
+    public SinhVienPoly(String hoTen, String nganh) 
+    {
         this.hoTen = hoTen;
         this.nganh = nganh;
     }
     abstract public double getDiem();
 
+    
     public String getHocLuc(){
         double diem = getDiem();
         String hocLuc;
-        if(diem<5) hocLuc = "Yeu";
-        else 
-            if(diem<6.5) hocLuc = "Trung binh";
+            if(diem<5) hocLuc = "Yeu";
             else 
-                if(diem<7.5) hocLuc = "Kha";
+                if(diem<6.5) hocLuc = "Trung binh";
                 else 
-                    if(diem<9) hocLuc = "Gioi";
-                    else hocLuc = "Xuat sac";
-
+                    if(diem<7.5) hocLuc = "Kha";
+                    else 
+                        if(diem<9) hocLuc = "Gioi";
+                        else hocLuc = "Xuat sac";
         return hocLuc;
     }
+    
+    
     public void Xuat(){
         System.out.println("\nThong tin sinh vien: " + hoTen);
-        System.out.println("Nganh: " + nganh + "\nDiem: " + getDiem() + "\nHoc luc: " + getHocLuc());
+        System.out.println("Nganh: " + nganh + "-- Diem: " + getDiem() + "-- Hoc luc: " + getHocLuc());
     }
-
 }
